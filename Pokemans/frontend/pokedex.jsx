7a624c39858b19e16e79917dcea0678b4e5f9746
chasemlim <chasemlim@gmail.com>
2018-12-05 17:15:54 -0800

    Finish Phase 3 and redux cycle

--- Pokemans/frontend/pokedex.jsx
+++ Pokemans/frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const rootEl = document.getElementById('root');
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
